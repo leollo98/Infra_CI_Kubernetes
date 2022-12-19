83e57c2f38c84a1995ca7c18070b2d2db3b0ae30
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 17:11:20 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -12,3 +12,21 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
+
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
