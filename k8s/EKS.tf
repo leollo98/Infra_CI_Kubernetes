1aa33f3d02243baab1199efe027bbbaf15ef4e9c
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 11:59:33 -0300

    Update EKS.tf

--- k8s/EKS.tf
+++ k8s/EKS.tf
@@ -3,7 +3,7 @@ module "eks" {
 
   cluster_name                    = var.cluster_name
   cluster_version                 = "1.24"
-  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
