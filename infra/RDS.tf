f9c8d087d0645d32d102f0bb04c67d30f9487575
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 00:16:28 -0300

    Update RDS.tf

--- infra/RDS.tf
+++ infra/RDS.tf
@@ -9,5 +9,4 @@ resource "aws_db_instance" "default" {
   publicly_accessible  = true
   db_subnet_group_name = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [aws_security_group.db.id]
-  depends_on = [module.eks]
 }
