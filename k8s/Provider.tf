3f05faae8394e47a987ed3bc838882012dcca4d1
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 16:39:18 -0300

    Update Provider.tf

--- k8s/Provider.tf
+++ k8s/Provider.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  region  = "us-west-2"
-}
-
-module infra {
-  source = "../infra"
-  
-  cluster_name = var.cluster_name
-}
-
 data "aws_eks_cluster" "default" {
   name = var.cluster_name
 }
