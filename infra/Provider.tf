ea972bdd990cefd72414de5b91416d634ffc8cf3
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-18 23:50:09 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -14,17 +14,14 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "default" {
-  depends_on = [module.eks]
   name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "default" {
-  depends_on = [module.eks]
   name = var.cluster_name
 }
 
 provider "kubernetes" {
-  depends_on = [data.aws_eks_cluster.default]
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
