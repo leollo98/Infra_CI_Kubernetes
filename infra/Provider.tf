eef3d287c314b3ac666aa8564e12b9341211d6fe
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 17:18:01 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -13,19 +13,9 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-data "aws_eks_cluster" "default" {
-  name = var.cluster_name
-  depends_on = [module.eks.kubernetes_config_map.aws_auth]
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = var.cluster_name
-  depends_on = [module.eks.kubernetes_config_map.aws_auth]
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
