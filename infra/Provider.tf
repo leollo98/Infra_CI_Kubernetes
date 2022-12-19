ff702436866670dd313b0b0336e568a552b5e645
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 17:12:44 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -15,10 +15,12 @@ provider "aws" {
 
 data "aws_eks_cluster" "default" {
   name = var.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "default" {
   name = var.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
