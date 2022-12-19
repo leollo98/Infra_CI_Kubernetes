dbfceddfac0fda722634a0a29535531eff7d3267
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-19 11:40:30 -0300

    Update Provider.tf

--- k8s/Provider.tf
+++ k8s/Provider.tf
@@ -25,7 +25,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
   }
