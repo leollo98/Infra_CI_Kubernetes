772e3ba24d4b4ecb882b8ae67e9334fa2233158d
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2022-12-20 15:14:43 -0300

    Update Provider.tf

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -22,3 +22,94 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+
+resource "kubernetes_deployment" "Go-API" {
+  metadata {
+    name = "go-api"
+    labels = {
+      nome = "Go"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        nome = "Go"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          nome = "Go"
+        }
+      }
+
+      spec {
+        container {
+          image = "leonardosartorello/go_ci:22"
+          name  = "Go"
+          
+          env {
+            name  = "HOST"
+            value = aws_db_instance.default.address
+          }
+          env {
+            name  = "PORT"
+            value = aws_db_instance.default.port
+          }
+          env {
+            name  = "USER"
+            value = aws_db_instance.default.username
+          }
+          env {
+            name  = "PASSWORD"
+            value = "rootroot"
+          }
+          env {
+            name  = "DBNAME"
+            value = aws_db_instance.default.db_name
+          }
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/bruno"
+              port = 8000
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "LoadBalancer" {
+  metadata {
+    name = "load-balancer-go-api"
+  }
+  spec {
+    selector = {
+      nome = "Go"
+    }
+    port {
+      port = 8000
+      target_port = 8000
+    }
+    type = "LoadBalancer"
+  }
+}
