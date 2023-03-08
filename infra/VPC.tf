d7b7f72d62499c715ba3f239c13079cd9630314d
thaysesf <92764190+thaysesf@users.noreply.github.com>
2023-03-08 14:25:58 -0300

    Update VPC.tf

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "VPC-EKS"
   cidr = "10.0.0.0/16" #10.0.1.1 - 10.0.255.255
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets= ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
